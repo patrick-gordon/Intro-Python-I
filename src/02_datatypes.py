67a5bc78a62dfea0c803abd0af500487dec967ec
patrick-gordon <patrickgordon42@yahoo.com>
2020-04-06 16:47:42 -0500

    first five done and comprehensions

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -11,12 +11,16 @@ on a string and an integer.
 x = 5
 y = "7"
 
+
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+z = x + int(y)
 
-print(f'{x} + {y}')
+print(z)
+
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+print(f'{x}{y}')
